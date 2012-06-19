8fca4aed3cd55b100f6bdb5094fb1a237b13dec4
elongeau <emmanuel.longeau@gmail.com>
2012-06-19 09:27:40 +0200

    minor corrections

--- library/src/fr/android/infinitelist/InfiniteListView.java
+++ library/src/fr/android/infinitelist/InfiniteListView.java
@@ -23,7 +23,7 @@ import fr.android.infinitelist.interfaces.OnListEnds;
 /*
  * ----------------------------------------------------------------------------
  * "THE BEER-WARE LICENSE" (Revision 42):
- * <elongeau@gmail.com> wrote this file. As long as you retain this notice you
+ * <longeaue@gmail.com> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return 
  * elongeau
